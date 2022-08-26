b3a6f57b5d5e96dac225b6be91418e45945641dc
Bryan Sharpley <motosharpley@gmail.com>
2022-08-25 21:28:50 -0700

    add some starter content ditch the boilerplate

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -46,7 +46,19 @@ const Resources: React.FC = () => {
           </IonItem>
 
           <IonCardContent>
-            <p>Card Content</p>
+            <p>
+              If there is just one thing you have to get good at to level up
+              your coding skills the most, I would say learning to read
+              documentation and then effectively implementing something using
+              the tech in the docs you just read. It doesn't sound all that
+              glamorous but practicing reading documentation and implementing
+              even just the example code will make you a better programmer every
+              time you do it. And it's 100% Free!
+            </p>
+            <p>
+              Stay Tuned on the DevPath YouTube channel for Walks through the
+              Docs.
+            </p>
           </IonCardContent>
         </IonCard>
       </IonContent>
